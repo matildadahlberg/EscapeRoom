88a771e10b2eba3daf067761b94370fce5d2dca0
Matilda Dahlberg <info.matildadahlberg@gmail.com>
2019-02-04 14:26:05 +0100

    Update: New message from Matilda

--- EscapeRoom/ViewController.swift
+++ EscapeRoom/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Message from MatildaB")
+        
+        print("New message from Matilda")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
