a0b515c8504b5dcbbe2e2afc89e5701d3243afa3
Matilda Dahlberg <info.matildadahlberg@gmail.com>
2019-02-04 14:17:43 +0100

    Edit: Print in view controller

--- EscapeRoom/ViewController.swift
+++ EscapeRoom/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Message from Matilda")
+        print("Message from MatildaA")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
