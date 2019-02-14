7a51dbc56edada04fd113d73e5ef7b622ccf6fd5
Matilda Dahlberg <info.matildadahlberg@gmail.com>
2019-02-14 15:53:11 +0100

    Add: New daresay font. Update: TurnIphoneController

--- EscapeRoom/Controller/TurnIphoneViewController.swift
+++ EscapeRoom/Controller/TurnIphoneViewController.swift
@@ -5,15 +5,8 @@ class TurnIphoneViewController: UIViewController {
     @IBOutlet weak var exitButton: UIButton!
     @IBOutlet weak var unlockButton: UIButton!
     
-    
     @IBOutlet weak var iphoneVertical: UIImageView!
-    
     @IBOutlet weak var arrow: UIImageView!
-    
-    @IBOutlet weak var iphoneHorizontal: UIImageView!
-    var change = true
-    
-
     
     var descriptionLabel = UILabel()
     var right = false
@@ -21,7 +14,7 @@ class TurnIphoneViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         exitButton.layer.cornerRadius = 15
         unlockButton.isHidden = true
         descriptionLabel = UILabel(frame: CGRect(x: self.view.frame.width/2 - 150, y: self.view.frame.height/2 - 50, width: 300, height: 100))
@@ -31,32 +24,21 @@ class TurnIphoneViewController: UIViewController {
         descriptionLabel.text = "Turn iPhone to the left and then back"
         self.view.addSubview(descriptionLabel)
         
-        iphoneHorizontal.isHidden = true
-       
-      
-//                arrow.isHidden = false
-        
-     
+        UIView.animate(withDuration: 3.0, delay: 0, options: [.repeat, .autoreverse],
+                       animations: { [unowned self] in
+                        self.iphoneVertical.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi/2))
+                        
+                        self.arrow.transform = CGAffineTransform(translationX: 28, y: 35)
+                        
+        }) { [unowned self] (finished: Bool) in
+            self.iphoneVertical.transform = CGAffineTransform.identity
+            self.arrow.transform = CGAffineTransform.identity
             
-            UIView.animate(withDuration: 2.0, delay: 0, options: [.repeat, .autoreverse],
-                           animations: { [unowned self] in
-
-
-                                    self.iphoneVertical.transform = CGAffineTransform(rotationAngle: CGFloat(Double.pi/2))
-//                                self.arrow.isHidden = true
-
-
-
-
-                }) { [unowned self] (finished: Bool) in
-//                    self.arrow.isHidden = true
-                    self.iphoneVertical.transform = CGAffineTransform.identity
-                    
-            }
-
+        }
+        
     }
     
-
+    
     
     
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
