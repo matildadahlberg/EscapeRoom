8903cac3ab5be9e554ba71068d748fc7b4e8e6f0
Niklas Lindell <niklas_lindell@hotmail.com>
2019-02-15 16:59:14 +0100

    Add: Images and segues

---
+++ EscapeRoom/Controller/PasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PasswordViewController.swift
+//  EscapeRoom
+//
+//  Created by Niklas Lindell on 2019-02-15.
+//  Copyright © 2019 Matilda Dahlberg. All rights reserved.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
