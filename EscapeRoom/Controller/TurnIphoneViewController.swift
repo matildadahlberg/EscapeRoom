8ddda548c3a74e9edf061767673199d2b14e9590
Matilda Dahlberg <info.matildadahlberg@gmail.com>
2019-02-15 15:33:55 +0100

    Add: Constraints on one view.

--- EscapeRoom/Controller/TurnIphoneViewController.swift
+++ EscapeRoom/Controller/TurnIphoneViewController.swift
@@ -7,6 +7,10 @@ class TurnIphoneViewController: UIViewController, ShowsAlert {
     @IBOutlet weak var arrow: UIImageView!
     @IBOutlet weak var timeLabel: UILabel!
     
+    @IBOutlet weak var handle: UIImageView!
+    
+    @IBOutlet weak var keyHole: UIImageView!
+    
     var descriptionLabel = UILabel()
     var right = false
     var updateTimeLabel = Timer()
@@ -15,6 +19,7 @@ class TurnIphoneViewController: UIViewController, ShowsAlert {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         updateTimeLabel = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
         
         UIView.animate(withDuration: 3.0, delay: 0, options: [.repeat, .autoreverse],
@@ -28,8 +33,9 @@ class TurnIphoneViewController: UIViewController, ShowsAlert {
             self.arrow.transform = CGAffineTransform.identity
             
         }
-        
     }
+    
+    
     
     
     
@@ -37,6 +43,11 @@ class TurnIphoneViewController: UIViewController, ShowsAlert {
     override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
         
         if UIDevice.current.orientation == .landscapeRight {
+            
+            keyHole.frame = CGRect(x: 330, y: 200, width: 41, height: 39)
+            handle.image = UIImage(named: "HandleDown")
+            handle.frame = CGRect(x: 330, y: 150, width: 170, height: 80)
+            
             right = true
         }
         if UIDevice.current.orientation == .portrait {
